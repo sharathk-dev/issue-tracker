9b215f1b04dc3709946f785e7411b834e8b3c7f5
sharathkuchal <sharathkuchal@gmail.com>
2025-10-03 22:01:16 +0530

    optimised middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,5 @@
-import { auth } from '@/auth';
-
-export default auth((req) => {
-  const isLoggedIn = !!req.auth;
-  const isWriteOperation = req.nextUrl.pathname.includes('/new') || req.nextUrl.pathname.includes('/edit');
-
-  if (!isLoggedIn && isWriteOperation) {
-    const newUrl = new URL('/auth/signin', req.nextUrl.origin);
-    newUrl.searchParams.set('callbackUrl', req.nextUrl.pathname);
-    return Response.redirect(newUrl);
-  }
-});
+export { auth as middleware } from '@/auth';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/issues/:path*'],
+  matcher: ['/issues/new', '/issues/:id/edit'],
 };
